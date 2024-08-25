10ff6a4a97a85b748a9dce4359616c1832a97f23
Gabriel Sinotti <gabrielsntt13@gmail.com>
2024-08-25 13:09:25 -0300

    SO's by Item Type ( Inheriting the Inventory Item SO)

---
+++ GabrielSinotti_Task/Assets/_Project/Logic/SO/Inventory/ConsumableItemSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Main.SO.Items
+{
+    [CreateAssetMenu(menuName = "SO/Interactable/Inventory Item/Consumable Item", fileName = "New Consumable Item")]
+    public class ConsumableItemSO : InventoryItemSO
+    {
+
+    }
+}
+
---
+++ GabrielSinotti_Task/Assets/_Project/Logic/SO/Inventory/ReadableItemSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Main.SO.Items
+{
+    [CreateAssetMenu(menuName = "SO/Interactable/Inventory Item/Readable Item", fileName = "New Readable Item")]
+    public class ReadableItemSO : InventoryItemSO
+    {
+        
+    }
+}
+
