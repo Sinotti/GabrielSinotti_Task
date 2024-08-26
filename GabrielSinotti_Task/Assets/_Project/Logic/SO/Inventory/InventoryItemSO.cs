d0d470680c04a78bf4fafa0b3eb70ca72afbaafd
Gabriel Sinotti <gabrielsntt13@gmail.com>
2024-08-26 05:19:51 -0300

    Update InventoryItemSO

--- GabrielSinotti_Task/Assets/_Project/Logic/SO/Inventory/InventoryItemSO.cs
+++ GabrielSinotti_Task/Assets/_Project/Logic/SO/Inventory/InventoryItemSO.cs
@@ -10,11 +10,9 @@ namespace Main.SO.Items
         [Space(6)]
 
         [SerializeField] private int _itemID;
-        [SerializeField] private string _itemName;
         [SerializeField] private Sprite _itemImage;
         [SerializeField] private InventoryItem _itemTypeBaseScript;
         public int ItemID { get => _itemID; set => _itemID = value; }
-        public string ItemName { get => _itemName; set => _itemName = value; }
         public Sprite ItemImage { get => _itemImage; set => _itemImage = value; }
         public InventoryItem ItemTypeBaseScript { get => _itemTypeBaseScript; set => _itemTypeBaseScript = value; }
     }
